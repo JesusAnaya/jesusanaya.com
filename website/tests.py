98325be9b074189f0fcfb8a81cbcf7a107453258
JesusAnaya <jesus.anaya.dev@gmail.com>
2013-09-22 12:10:10 -0700

    Add selenium tests

--- website/tests.py
+++ website/tests.py
@@ -1,16 +1,28 @@
 """
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+
+    def test_home(self):
         """
-        Tests that 1 + 1 always equals 2.
         """
-        self.assertEqual(1 + 1, 2)
+        self.driver.get("http://127.0.0.1:9000/")
+        self.driver.find_element_by_class_name("home").click()
+        time.sleep(2)
+
+        self.assertIn("Web development with python and more", self.driver.title)
+
+        self.driver.find_element_by_class_name("resume").click()
+        time.sleep(2)
+
+        self.assertIn("Jesus Anaya Skills", self.driver.title)
+
+    def tearDown(self):
+        self.driver.close()
